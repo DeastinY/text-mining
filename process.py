bc463edd3372ff3ec5f521341f3441d1279f5187
DeastinY <polzin.richard@gmail.com>
2017-05-07 16:04:17 +0200

    Implemented InformationMiner and added pop lyrics

--- process.py
+++ process.py
@@ -5,4 +5,5 @@ if __name__ == '__main__':
     INPUT = "preprocessing/lyrics.json"
     with open(INPUT, 'r') as fin:
         lyrics = json.load(fin)
-    im = InformationMiner([l["text_raw"] for l in lyrics], save_output=True)
+    im = InformationMiner([l["text_raw"] for l in lyrics], save_output=True, outdir='out_pop')
+    im.process()
